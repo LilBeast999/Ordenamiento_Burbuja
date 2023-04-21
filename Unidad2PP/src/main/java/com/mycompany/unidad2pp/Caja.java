f551ad3ccb5eaecb0e2b7d12dc61fb1c16cda6b8
Gustavo T <gustatorres132@gmail.com>
2023-04-21 18:35:14 -0400

    Unidad 1 ordenamiento insercion

---
+++ Unidad2PP/src/main/java/com/mycompany/unidad2pp/Caja.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.unidad2pp;
+
+/**
+ *
+ * @author gusta
+ */
+public class Caja {
+    int peso;
+    int posicionx,posiciony;
+        
+    public Caja(int peso) {
+        this.peso=peso;
+    }
+    
+    public int getPeso(){
+        return this.peso;
+    }
+    
+    public int getPosicionx(){
+        return this.posicionx;
+    }
+    
+    public int getPosiciony(){
+        return this.posiciony;
+    }
+}
+
+
+
+    
+
+
---
+++ Unidad2PP/src/main/java/com/mycompany/unidad2pp/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.mycompany.unidad2pp;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
