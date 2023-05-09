091dfc0f75b983fc7db0f30f28ffc49df980dffb
Thomas Francisco Lizana Sepulveda <thomasfranciscolizanasepulveda@gmail.com>
2023-05-09 15:39:18 -0400

    Testeo algoritmo cocktail

--- Unidad2PP/src/main/java/com/mycompany/unidad2pp/Ordenamientos.java
+++ Unidad2PP/src/main/java/com/mycompany/unidad2pp/Ordenamientos.java
@@ -516,17 +516,17 @@ public class Ordenamientos {
     public void Cocktail (ArrayList<Integer> arreglo, int numerodecajas, ArrayList<AnchorPane> cajasAnchor,AnchorPane anchor){
         boolean swapped = true;
     int start = 0;
-    int end = arr.length - 1;
+    int end = arreglo.size() - 1;
 
     while (swapped) {
         swapped = false;
 
         // Mover elementos grandes al final
         for (int i = start; i < end; i++) {
-            if (arr[i] > arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
+            if (arreglo.get(i) > arreglo.get(i + 1)) {
+                int temp = arreglo.get(i);
+                arreglo.set(i, arreglo.get(i + 1));
+                arreglo.set(i + 1, temp);
                 swapped = true;
             }
         }
@@ -534,14 +534,15 @@ public class Ordenamientos {
 
         // Mover elementos pequeños al inicio
         for (int i = end; i > start; i--) {
-            if (arr[i] < arr[i - 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i - 1];
-                arr[i - 1] = temp;
+            if (arreglo.get(i) < arreglo.get(i - 1)) {
+                int temp = arreglo.get(i);
+                arreglo.set(i, arreglo.get(i - 1));
+                arreglo.set(i - 1, temp);
                 swapped = true;
             }
         }
         start++;
+    }
     }
         
     }
