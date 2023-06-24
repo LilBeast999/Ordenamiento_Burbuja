c3d861ee74125d14d2ec803edcec8791f0ef94f0
Thomas Francisco Lizana Sepulveda <thomasfranciscolizanasepulveda@gmail.com>
2023-06-24 17:03:20 -0400

    Inicio de intentar añadir arboles entre las autopistas

--- Unidad2PP/src/main/java/com/mycompany/unidad2pp/Lapiz.java
+++ Unidad2PP/src/main/java/com/mycompany/unidad2pp/Lapiz.java
@@ -918,29 +918,38 @@ this.anchor.getChildren().add(autopista);
 
 // Añadir líneas divisoras de la autopista
 for (int i = 0; i < 100; i++) {
-    Rectangle lineaDivisora = new Rectangle(-5000 + (100 * i), 730, 60, 5);
+    Rectangle lineaDivisora = new Rectangle(-5000 + (100 * i), 735, 60, 5);
     lineaDivisora.setFill(Color.WHITE);
     this.anchor.getChildren().add(lineaDivisora);
 }
-// Añadir arcenes a la autopista
-Rectangle arcenIzquierdo = new Rectangle(-5000, 700, 200, 80);
-arcenIzquierdo.setFill(Color.GRAY); // Color del arcén izquierdo
-this.anchor.getChildren().add(arcenIzquierdo);
-
-Rectangle arcenDerecho = new Rectangle(-4800, 700, 200, 80);
-arcenDerecho.setFill(Color.GRAY); // Color del arcén derecho
-this.anchor.getChildren().add(arcenDerecho);
-Rectangle autopista2 = new Rectangle(-5000, 850, 20000, 80);
+Rectangle autopista2 = new Rectangle(-5000, 855, 20000, 80);
 autopista2.setFill(Color.LIGHTGRAY); // Color de la autopista
 this.anchor.getChildren().add(autopista2);
 
 // Añadir líneas divisoras de la autopista 2
 for (int i = 0; i < 200; i++) {
-    Rectangle lineaDivisora = new Rectangle(-5000 + (100 * i), 880, 60, 5);
+    Rectangle lineaDivisora = new Rectangle(-5000 + (100 * i), 890, 60, 5);
     lineaDivisora.setFill(Color.WHITE);
     this.anchor.getChildren().add(lineaDivisora);
 }
+// Añadir árboles entre las autopistas
+// Añadir árboles entre las autopistas
+double treeRadius = 10; // Radio de los árboles
+double treeGap = 100; // Espacio entre los árboles
 
+for (double x = -5000 + treeRadius; x < 5000; x += treeGap) {
+    for (double y = 780; y <= 840; y += treeGap) {
+        Circle arbol = new Circle(x, y, treeRadius);
+        arbol.setFill(Color.DARKGREEN);
+        this.anchor.getChildren().add(arbol);
+    }
+}
+
+for (int i = 0; i < 100; i++) {
+    Rectangle arbol = new Rectangle(-5000 + (100 * i), 840, 20, 75);
+    arbol.setFill(Color.DARKGREEN);
+    this.anchor.getChildren().add(arbol);
+}
 
      return this.anchor;
     }
